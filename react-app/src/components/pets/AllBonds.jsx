d8696ceb0c009b6d2a406c82e982691bdb3ff872
xavierlwr <xlwr1g20@soton.ac.uk>
2023-08-03 15:34:07 +0100

    AllBonds Dropdown Sort
    Dropdown sorts ASC or DESC by Date, ID, Type

--- react-app/src/components/pets/AllBonds.jsx
+++ react-app/src/components/pets/AllBonds.jsx
@@ -1,71 +1,94 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import { BondDetail } from "./BondDetail";
 import { getAllBonds } from '../../services/BondServices';
-import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const AllBonds = () => {
-  const [Bonds, setBonds] = useState([]);
-  const [sortOrder, setSortOrder] = useState('asc'); // Track the sorting order
+    const [bonds, setBonds] = useState([]);
+    const [sortOrder, setSortOrder] = useState('asc_id');
 
-  useEffect( () => {
-    getBondsFromAPI();
-  }, []);
-  
-  const getBondsFromAPI = ()=>{
-    getAllBonds()
-    .then(res => {
-        console.log(res.data)
-        setBonds(res.data);
-    })
-    .catch(err => {
-        setBonds([]);
-        console.log(err);
-    })
-  }
+    useEffect(() => {
+        getBondsFromAPI();
+    }, []);
+
+    const getBondsFromAPI = () => {
+        getAllBonds()
+            .then(res => {
+                console.log(res.data)
+                setBonds(res.data);
+            })
+            .catch(err => {
+                setBonds([]);
+                console.log(err);
+            })
+    }
+
+    const handleSortClick = (order) => {
+        const sortedBonds = [...bonds].sort((a, b) => {
+            if (order === 'asc_id') {
+                return a.bondId - b.bondId;
+            } else if (order === 'desc_id') {
+                return b.bondId - a.bondId;
+            } else if (order === 'asc_date') {
+                return new Date(a.bondMaturityDate) - new Date(b.bondMaturityDate);
+            } else if (order === 'desc_date') {
+                return new Date(b.bondMaturityDate) - new Date(a.bondMaturityDate);
+            } else if (order === 'type') {
+            return a.type.localeCompare(b.type);}
+            return 0;
+        });
+
+        setBonds(sortedBonds);
+        setSortOrder(order);
+    }
 
 
-  const handleSortClick = () => {
-    const sortedBonds = [...Bonds].sort((a, b) => {
-      if (sortOrder === 'asc') {
-        return a.bondId - b.bondId;
-      } else {
-        return b.bondId - a.bondId;
-      }
-    });
+    return (
+        <>
+            <div className="list-group text-center">
+                <div className="d-flex justify-content-between subnav">
+                    <h5 style={{ marginLeft: "4.5%", marginTop: "1%" }}>All Active Bonds</h5>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="primary" className="btn btn-secondary" id="sort">
+                            Sort: {sortOrder === 'asc_id'
+                            ? 'Ascending ID'
+                            : sortOrder === 'desc_id'
+                                ? 'Descending ID'
+                                : sortOrder === 'asc_date'
+                                    ? 'Ascending Date'
+                                    : sortOrder === 'desc_date'
+                                        ? 'Descending Date'
+                                        : 'Bond Type'}
+                        </Dropdown.Toggle>
 
-    setBonds(sortedBonds);
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => handleSortClick('asc_id')}>Ascending ID</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSortClick('desc_id')}>Descending ID</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSortClick('asc_date')}>Ascending Date</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSortClick('desc_date')}>Descending Date</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSortClick('type')}>Bond Type</Dropdown.Item>
 
-    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
-  }
-
-
-
-  return (
-    <>
-    <div className="list-group text-center">
-        <div className="d-flex justify-content-between subnav">
-          <h5 style={{marginLeft: "4.5%", marginTop: "1%"}}>All Active Bonds</h5>
-          <Button variant="primary" className="btn btn-secondary" id="sort" onClick={handleSortClick}>Sort</Button>
-        </div>
-      </div>
-    <h4 style={{marginLeft: "4.5%", marginTop:"2%", marginBottom: "1%"}}> Total count is: <small className="text-body-secondary">{Bonds.length}</small></h4>
-
-    <Container fluid="md-4" style={{marginLeft: "4%", marginTop: "2%", width: "95%"}} >
-    {/* <Row xs={4}> */}
-    <Row md={5}>
-    {Bonds.map(bond => (
-      <Col>
-      <div className='container' key={bond.bondId.toString()}>
-        <BondDetail info={bond}/>
-      </div></Col>
-     ))}
-    </Row></Container>
-  </>
-   
-  )
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+            </div>
+            <h4 style={{ marginLeft: "4.5%", marginTop: "2%", marginBottom: "1%" }}> Total count is: <small className="text-body-secondary">{bonds.length}</small></h4>
+            <Container fluid="md-4" style={{ marginLeft: "4%", marginTop: "2%", width: "95%" }} >
+                <Row md={5}>
+                    {bonds.map(bond => (
+                        <Col key={bond.bondId.toString()}>
+                            <div className='container'>
+                                <BondDetail info={bond} />
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </>
+    )
 }
 
-export default AllBonds
+export default AllBonds;
