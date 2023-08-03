94c56d5cf97db4ff45de7904a7f40424c1dd00cd
Ivaylo Spasov <st151999@stud.uni-stuttgart.de>
2023-08-03 10:48:50 +0200

    add BondService constructor

--- java-api/src/main/java/com/db/grad/javaapi/controller/BondController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/BondController.java
@@ -2,6 +2,7 @@ package com.db.grad.javaapi.controller;
 
 import com.db.grad.javaapi.model.Bond;
 import com.db.grad.javaapi.service.BondService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,17 @@ import java.util.List;
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "http://localhost:3000")
 public class BondController {
+
+    @Autowired
     private BondService bondService;
 
+    @Autowired
+    public BondController(BondService bondService) {
+        this.bondService = bondService;
+    }
+
     @GetMapping("/bonds")
-    public List<Bond> getAllDogs() {
+    public List<Bond> getAllBonds() {
         return bondService.getAllBonds();
     }
 }
