419d03b2d58cafcc615d86242f968da5ab94a9e3
Ivaylo Spasov <st151999@stud.uni-stuttgart.de>
2023-08-03 12:28:44 +0200

    Add endpoints

--- java-api/src/main/java/com/db/grad/javaapi/service/BondHolderService.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/BondHolderService.java
@@ -1,11 +1,14 @@
 package com.db.grad.javaapi.service;
 
+import com.db.grad.javaapi.model.Bond;
 import com.db.grad.javaapi.model.BondHolder;
+import com.db.grad.javaapi.model.Trade;
 import com.db.grad.javaapi.repository.BondHolderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BondHolderService {
@@ -18,15 +21,28 @@ public class BondHolderService {
         this.bondHolderRepository = bondHolderRepository;
     }
 
-    public BondHolder getBondById(Integer bondHolderId) {
-        if (bondHolderRepository.existsById(bondHolderId)) {
-            return bondHolderRepository.getReferenceById(bondHolderId);
-        } else {
-            return null;
-        }
+    public BondHolder addBondHolder(BondHolder bondHolder) {
+        return bondHolderRepository.save(bondHolder);
     }
 
-    public List<BondHolder> getAllBonds() {
+    public BondHolder getBondHolderById(Integer bondHolderId) {
+        Optional<BondHolder> bondHolder = bondHolderRepository.findById(bondHolderId);
+        return bondHolder.orElse(null);
+    }
+
+    public List<BondHolder> getAllBondHolders() {
         return bondHolderRepository.findAll();
+    }
+
+    public boolean deleteBondHolderById(Integer bondHolderId) {
+        boolean deleted = false;
+
+        Optional<BondHolder> bondHolder = bondHolderRepository.findById(bondHolderId);
+        if (bondHolder.isPresent()) {
+            bondHolderRepository.delete(bondHolder.get());
+            deleted = true;
+        }
+
+        return deleted;
     }
 }
