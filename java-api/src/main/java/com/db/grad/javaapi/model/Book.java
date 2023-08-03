e7f2c774fb29fc884cd6ff25a484e18995bfc212
Ivaylo Spasov <st151999@stud.uni-stuttgart.de>
2023-08-03 09:54:01 +0200

    Merge backend

---
+++ java-api/src/main/java/com/db/grad/javaapi/model/Book.java
@@ -0,0 +1,37 @@
+package com.db.grad.javaapi.model;
+
+import javax.persistence.*;
+import java.lang.annotation.Native;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Integer bookId;
+
+    @Column(nullable = false)
+    private String bookName;
+
+    @ManyToMany(mappedBy = "books")
+    private Set<User> users = new HashSet<>();
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+}
