e4e822833d86b8371293c000141f5ba23688b220
xavierlwr <xlwr1g20@soton.ac.uk>
2023-08-07 11:34:21 +0100

    Date search
    + alerts

--- react-app/src/components/pets/AllBonds.jsx
+++ react-app/src/components/pets/AllBonds.jsx
@@ -6,7 +6,8 @@ import { BondDetail } from "./BondDetail";
 import { getAllBonds } from '../../services/BondServices';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
-
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
 const AllBonds = () => {
     const [bonds, setBonds] = useState([]);
@@ -14,14 +15,22 @@ const AllBonds = () => {
     const [sortOrder, setSortOrder] = useState('asc_id');
     const [showPrev, setShowPrev] = useState(false);
     const [showNext, setShowNext] = useState(false);
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [dateInput, setDateInput] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
 
     useEffect(() => {
         getBondsFromAPI();
     }, []);
 
     useEffect(() => {
+        setCurrentDate(new Date());
+    }, []);
+
+    useEffect(() => {
         filterBonds();
-    }, [showPrev, showNext]);
+    }, [showPrev, showNext, currentDate]);
+
 
 
     const getBondsFromAPI = () => {
@@ -37,10 +46,11 @@ const AllBonds = () => {
             })
     }
 
-
+    const handleAlertClose = () => {
+        setShowAlert(false);
+    };
 
     const handleSortClick = (order) => {
-
         const sortedBonds = [...bonds].sort((a, b) => {
             if (order === 'asc_id') {
                 return a.bondId - b.bondId;
@@ -51,7 +61,7 @@ const AllBonds = () => {
             } else if (order === 'desc_date') {
                 return new Date(b.bondMaturityDate) - new Date(a.bondMaturityDate);
             } else if (order === 'type') {
-            return a.type.localeCompare(b.type);}
+                return a.type.localeCompare(b.type);}
             return 0;
         });
 
@@ -69,15 +79,38 @@ const AllBonds = () => {
         filterBonds()
     }
 
+    const handleDateInputChange = (event) => {
+        setDateInput(event.target.value);
+    }
+
+    const handleDateSubmit = () => {
+        const inputDate = new Date(dateInput);
+        if (!isNaN(inputDate)) {
+            setShowAlert(true);
+            setCurrentDate(inputDate);
+            filterBonds(inputDate);
+            setTimeout(() => {
+                setShowAlert(false);
+            }, 3000);
+        }
+    }
+
+    const handleReset = () => {
+        setCurrentDate(new Date());
+        setDateInput('');
+        filterBonds(new Date());
+        setShowAlert(true);
+        setTimeout(() => {
+            setShowAlert(false);
+        }, 3000);
+    }
+
     const filterBonds = () => {
-
-
         const filteredBonds = bondData.filter((bond) => {
-            const dateOffset = (24*60*60*1000) * 5;
-            const currentDate = new Date();
-            const prev5Date = new Date(currentDate - dateOffset);
-            const next5Date = new Date(currentDate + dateOffset)
 
+            const dateOffset = (24 * 60 * 60 * 1000) * 5;
+            const prev5Date = new Date(currentDate - dateOffset);
+            const next5Date = new Date(currentDate.getTime() + dateOffset);
             const bondDate = new Date(bond.bondMaturityDate);
 
             const isPrev = bondDate > prev5Date && bondDate < currentDate;
@@ -99,30 +132,42 @@ const AllBonds = () => {
 
 
 
-
-
     return (
         <>
             <div className="list-group text-center">
                 <div className="d-flex justify-content-between subnav">
-                    <h5 style={{ marginLeft: "4.5%", marginTop: "1%" }}>All Active Bonds</h5>
-                    <span style={{ marginLeft: "45%", textAlign: "left"}}>
-                    
-                    <Form.Check
-                        type="checkbox"
-                        label="Matured in Previous 5 Days"
-                        checked={showPrev}
-                        onChange={handleShowPrev}
-                        style={{ marginLeft: "10px", marginTop: "1%" }}
-                    />
-                    <Form.Check
-                        type="checkbox"
-                        label="Matures in Next 5 Days"
-                        checked={showNext}
-                        onChange={handleShowNext}
-                        style={{ marginLeft: "10px", marginTop: "1%" }}
-                    /></span>
-                    <Dropdown style={ {marginTop: "0.5%"} }>
+                    <h5 style={{ marginLeft: "4.5%", marginTop: "1%", marginBottom: "1%" }}>All Active Bonds</h5>
+
+                    <div className="d-flex align-items-center"> {/* Flex container */}
+                        <Form.Control
+                            type="date"
+                            value={dateInput}
+                            onChange={handleDateInputChange}
+                            style={{ marginRight: "10px", width: "150px" }}
+                        />
+                        <Button variant="primary" onClick={handleDateSubmit} style={{ marginRight: "10px" }}>Change Date</Button>
+                        <Button variant="secondary" onClick={handleReset}>Reset</Button>
+                    </div>
+
+
+                    <span style={{ marginLeft: "4%", textAlign: "left", marginTop: "1%" }}>
+        <Form.Check
+            type="checkbox"
+            label="Matured in Previous 5 Days"
+            checked={showPrev}
+            onChange={handleShowPrev}
+            style={{ marginRight: "10px" }}
+        />
+        <Form.Check
+            type="checkbox"
+            label="Matures in Next 5 Days"
+            checked={showNext}
+            onChange={handleShowNext}
+            style={{ marginRight: "10px" }}
+        />
+      </span>
+
+                    <Dropdown style={{ marginTop: "1%", marginRight: "10px" }}>
                         <Dropdown.Toggle variant="primary" className="btn btn-secondary" id="sort">
                             Sort: {sortOrder === 'asc_id'
                             ? 'Ascending ID'
@@ -134,24 +179,28 @@ const AllBonds = () => {
                                         ? 'Descending Date'
                                         : 'Bond Type'}
                         </Dropdown.Toggle>
-
                         <Dropdown.Menu>
                             <Dropdown.Item onClick={() => handleSortClick('asc_id')}>Ascending ID</Dropdown.Item>
                             <Dropdown.Item onClick={() => handleSortClick('desc_id')}>Descending ID</Dropdown.Item>
                             <Dropdown.Item onClick={() => handleSortClick('asc_date')}>Ascending Date</Dropdown.Item>
                             <Dropdown.Item onClick={() => handleSortClick('desc_date')}>Descending Date</Dropdown.Item>
                             <Dropdown.Item onClick={() => handleSortClick('type')}>Bond Type</Dropdown.Item>
-
                         </Dropdown.Menu>
                     </Dropdown>
-                    
                 </div>
             </div>
+
+            <div style={{ position: "relative" }}>
+                <Alert show={showAlert} variant="success" onClose={handleAlertClose} dismissible style={{ marginTop: '1%', position: "absolute", right: 0, zIndex: 9999 }}>
+                    Date changed successfully!
+                </Alert>
+            </div>
+
             <h4 style={{ marginLeft: "4.5%", marginTop: "2%", marginBottom: "1%" }}> Total count is: <small className="text-body-secondary">{bonds.length}</small></h4>
-            <Container fluid="md-4" style={{ marginLeft: "4%", marginTop: "2%", width: "95%" }} >
-                <Row md={5}>
+            <Container fluid style={{ marginLeft: "4%", marginTop: "2%", marginBottom: "2%" }} >
+                <Row>
                     {bonds.map(bond => (
-                        <Col key={bond.bondId.toString()}>
+                        <Col md={6} lg={4} xl={3} key={bond.bondId.toString()} style={{ marginBottom: "20px" }}>
                             <div className='container'>
                                 <BondDetail info={bond} />
                             </div>
@@ -160,7 +209,10 @@ const AllBonds = () => {
                 </Row>
             </Container>
         </>
+
     )
+
+
 }
 
 export default AllBonds;
